fe3d117f4a208aa740f576c4167986e50eff4645
leventelibal <leventelibal@gmail.com>
2019-08-24 12:39:41 -0400

    remove space

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("pull repo "){
-        properties([parameters([string(defaultValue: '54.89.213.223', description: '''Dev:54.89.213.223 QA:34.207.157.146 Prod:54.167.194.28''', name: 'Remote_instance ', trim: false)])])
+        properties([parameters([string(defaultValue: '54.89.213.223', description: '''Dev:54.89.213.223 QA:34.207.157.146 Prod:54.167.194.28''', name: 'Remote_instance', trim: false)])])
         git 'https://github.com/leventelibal/jenkins_repo.git'
     }
     stage("Install Apache"){
