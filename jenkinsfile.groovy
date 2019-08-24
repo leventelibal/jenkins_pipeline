f428da2fc92c323f5ed51834a05f566aeae61331
leventelibal <leventelibal@gmail.com>
2019-08-24 12:07:37 -0400

    add new parameter

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("say Hello"){
-        properties([pipelineTriggers([cron('* * * * *')])])
+        properties([parameters([string(defaultValue: 'Levent', description: 'please add your name ', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
         sh "echo hello${NAME}",
     }
 }
