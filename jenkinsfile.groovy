26c4d876df5929c6f6da0229eb606ebcb8c6908c
leventelibal <leventelibal@gmail.com>
2019-08-24 12:35:15 -0400

    add new

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,6 +4,16 @@ node {
         git 'https://github.com/leventelibal/jenkins_repo.git'
     }
     stage("Install Apache"){
-        sh "ssh centos@${Remote_instances} sudo yum install httpd -y"
+        sh "ssh centos@${Remote_instance} sudo yum install httpd -y"
     }
+    stage("Create index.html"){
+        sh "scp index.html centos@${Remote_instance}:/tmp"
+    }
+    stage("Move Files"){
+        sh "ssh centos@${Remote_instance} sudo mv /tmp/index.html /var/www/html/index.html"
+    }
+    stage("Restart http"){
+        sh "ssh centos@${Remote_instance} sudo systemctl start httpd"
+    }
+    
 }
