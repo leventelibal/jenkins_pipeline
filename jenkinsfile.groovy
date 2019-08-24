9e55af68d633af6b1a13da68191cfd7460a8d436
leventelibal <leventelibal@gmail.com>
2019-08-24 12:53:46 -0400

    remove s

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,15 +4,15 @@ node {
         git 'https://github.com/leventelibal/jenkins_repo.git'
     }
     stage("Install Apache"){
-        sh "ssh   ec2-user@${Remote_instances}    sudo yum install httpd -y"
+        sh "ssh   ec2-user@${Remote_instance}    sudo yum install httpd -y"
     }
     stage("Create Index.html"){
-        sh "scp  index.html  ec2-user@${Remote_instances}:/tmp"
+        sh "scp  index.html  ec2-user@${Remote_instance}:/tmp"
     }
     stage("Move Files"){
-        sh "ssh   ec2-user@${Remote_instances}    sudo mv /tmp/index.html  /var/www/html/index.html"
+        sh "ssh   ec2-user@${Remote_instance}    sudo mv /tmp/index.html  /var/www/html/index.html"
     }
     stage("Restart httpd"){
-        sh "ssh   ec2-user@${Remote_instances} sudo systemctl restart  httpd"
+        sh "ssh   ec2-user@${Remote_instance} sudo systemctl restart  httpd"
     }
 }
