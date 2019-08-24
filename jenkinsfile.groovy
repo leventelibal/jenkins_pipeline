3d2bd9edd4eeb170235f57fd8c672f5990f60be2
leventelibal <leventelibal@gmail.com>
2019-08-24 12:12:36 -0400

    add git

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,5 @@
 node {
     stage("say Hello"){
-        properties([parameters([string(defaultValue: 'Levent', description: 'please add your name ', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
-        sh "echo hello${NAME}",
-     }
+        https://github.com/leventelibal/jenkins_repo.git
+    }
 }
