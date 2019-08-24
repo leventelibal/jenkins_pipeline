4e29ec86e377a24dca43d687d4ad9c8387b2f9e4
leventelibal <leventelibal@gmail.com>
2019-08-24 12:52:54 -0400

    corerrected

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,19 +1,18 @@
 node {
-    stage("pull repo "){
-       properties([parameters([string(defaultValue: '54.89.213.223', description: '''Dev:54.89.213.223 QA:34.207.157.146 Prod:54.167.194.28''', name: 'Remote_instance', trim: false)])])
-       git 'https://github.com/leventelibal/jenkins_repo.git'
+    stage("Pull Repo"){
+        properties([parameters([string(defaultValue: '54.89.213.223', description: '''Dev:54.89.213.223 QA:34.207.157.146 Prod:54.167.194.28''', name: 'Remote_instance ', trim: false)])])
+        git 'https://github.com/leventelibal/jenkins_repo.git'
     }
     stage("Install Apache"){
-        sh "ssh centos@${Remote_instance} sudo yum install httpd -y"
+        sh "ssh   ec2-user@${Remote_instances}    sudo yum install httpd -y"
     }
-    stage("Create index.html"){
-        sh "scp index.html centos@${Remote_instance}:/tmp"
+    stage("Create Index.html"){
+        sh "scp  index.html  ec2-user@${Remote_instances}:/tmp"
     }
     stage("Move Files"){
-        sh "ssh centos@${Remote_instance} sudo mv /tmp/index.html /var/www/html/index.html"
+        sh "ssh   ec2-user@${Remote_instances}    sudo mv /tmp/index.html  /var/www/html/index.html"
     }
-    stage("Restart http"){
-        sh "ssh centos@${Remote_instance} sudo systemctl start httpd"
+    stage("Restart httpd"){
+        sh "ssh   ec2-user@${Remote_instances} sudo systemctl restart  httpd"
     }
-    
 }
